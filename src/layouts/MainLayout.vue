eadb5b7abceb7aa41daabd2737a094c0e6277c52
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-03 20:58:37 -0300

    fix:correcao conflito menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,68 +2,74 @@
   <q-layout view="hHh LpR fFf">
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn 
+        <q-btn
           v-if="authStore.isAuthenticated"
-          dense 
-          flat 
-          round 
-          icon="menu" 
-          @click="toggleLeftDrawer" 
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleLeftDrawer"
         />
-        
+       
         <q-toolbar-title>
           <img src="../assets/FlexPC.png" alt="Logo" style="width: 120px;" >
-
         </q-toolbar-title>
-
         <div v-if="authStore.isAuthenticated" class="user-info">
           <span class="user-name">{{ authStore.user?.name || authStore.user?.email }}</span>
-          <q-btn 
+          <q-btn
             v-if="authStore.isAdmin"
-            dense 
-            flat 
+            dense
+            flat
             size="sm"
             label="ADMIN"
             class="admin-badge q-ml-sm"
           />
-          <q-btn 
-            dense 
-            flat 
-            icon="logout" 
+          <q-btn
+            dense
+            flat
+            icon="logout"
             @click="handleLogout"
             class="q-ml-sm"
           >
             <q-tooltip>Sair</q-tooltip>
           </q-btn>
         </div>
-
-        <q-btn 
+        <q-btn
           v-if="authStore.isAuthenticated"
-          dense 
-          flat 
-          round 
-          icon="menu" 
-          @click="toggleRightDrawer" 
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleRightDrawer"
         />
       </q-toolbar>
     </q-header>
-
-    <q-drawer 
+    <q-drawer
       v-if="authStore.isAuthenticated"
-      show-if-above 
-      v-model="leftDrawerOpen" 
-      side="left" 
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
       bordered
     >
       <q-list>
         <q-item-label header>Links</q-item-label>
-        <q-item clickable v-ripple @click="$router.push('/')">
+        <!-- Usando o array linksList -->
+        <q-item 
+          clickable 
+          v-ripple 
+          v-for="link in linksList" 
+          :key="link.title"
+          @click="$router.push(link.link)"
+        >
           <q-item-section avatar>
-            <q-icon name="home" />
+            <q-icon :name="link.icon" />
           </q-item-section>
-          <q-item-section>Home</q-item-section>
+          <q-item-section>
+            <q-item-label>{{ link.title }}</q-item-label>
+            <q-item-label caption>{{ link.caption }}</q-item-label>
+          </q-item-section>
         </q-item>
-
+        <!-- Produtos separado se necessário -->
         <q-item clickable v-ripple @click="$router.push('/produto')">
           <q-item-section avatar>
             <q-icon name="shopping_bag" />
@@ -73,27 +79,17 @@
             <q-item-label caption>Ver todos os produtos</q-item-label>
           </q-item-section>
         </q-item>
-
       </q-list>
     </q-drawer>
-
-    <q-drawer 
+    <q-drawer
       v-if="authStore.isAuthenticated"
-      show-if-above 
-      v-model="rightDrawerOpen" 
-      side="right" 
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
       bordered
     >
-      <q-list>
-        <q-item-label header>Carrinho</q-item-label>
-        <q-item>
-          <q-item-section>
-            <q-item-label>Carrinho vazio</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
+    <!-- carrinho? -->
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -135,11 +131,11 @@ export default {
       linksList
     }
   },
-  
+ 
   created() {
     this.authStore = useAuthStore()
   },
-  
+ 
   methods: {
     toggleLeftDrawer() {
       this.leftDrawerOpen = !this.leftDrawerOpen
@@ -162,12 +158,10 @@ export default {
   display: flex;
   align-items: center;
 }
-
 .user-name {
   font-size: 0.9rem;
   margin-right: 0.5rem;
 }
-
 .admin-badge {
   background-color: rgba(255, 255, 255, 0.2);
   font-size: 0.7rem;
